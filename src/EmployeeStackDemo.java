b79ac40dfab78e9f4590dd061c9c9397d395208c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-23 10:44:55 +0400

    added stack name   to  class  EmployeeStackDemo

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -2,7 +2,8 @@ import java.util.Stack;
 
 public class EmployeeStackDemo {
     public static void main(String[] args) {
-        Stack<String> employeeStack
+        Stack<String> employeeStack =new Stack<>();
+
 
     }
 }
