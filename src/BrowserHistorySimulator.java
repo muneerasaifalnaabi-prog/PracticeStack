2172307b40ef810867573aace7dfb4976b523a36
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-23 11:44:51 +0400

    Create stack with string data type and named BrowserHistorySimulator

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class BrowserHistorySimulator {
     public static void main(String[] args) {
-        Stack<String>
+        Stack<String> browserHistorySimulator = new Stack<>();
 
 
     }
