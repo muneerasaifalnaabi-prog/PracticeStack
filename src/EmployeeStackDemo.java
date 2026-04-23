c4ba290422850ee4ce57c1f840c59992994de5b8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-23 10:53:00 +0400

    Completed Task1 EmployeeStackDemo

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -4,7 +4,7 @@ public class EmployeeStackDemo {
     public static void main(String[] args) {
         //Create a Stack
         Stack<String> employeeStack =new Stack<>();
-//push 5 element to stack
+        //push 5 element to stack
         employeeStack.push("Ishaq");
         employeeStack.push("Fatima");
         employeeStack.push("Ahmed");
@@ -15,7 +15,7 @@ public class EmployeeStackDemo {
         System.out.println("All Employee :" + employeeStack);
             // add one more element to stack
         employeeStack.push("Abdullah");
-//display all elements in stack after added one more
+        //display all elements in stack after added one more
         System.out.println("All Employees after added employee");
 
 
