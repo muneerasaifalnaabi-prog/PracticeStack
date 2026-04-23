6a128f9403cc4ab559d0145f9bcdd17c70e95e91
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-23 12:22:05 +0400

    Created stack with string datatype and named parenthesesValidator

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -1,5 +1,10 @@
+import java.util.Stack;
+
 public class ParenthesesValidator {
     public static void main(String[] args) {
+        //create stack
+        Stack<String > parenthesesValidator = new Stack<>();
+
 
     }
 
