514552e5579b77be7b2f03e15d2299a464a1e76b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-23 11:54:57 +0400

    Added comment

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -2,7 +2,11 @@ import java.util.Stack;
 
 public class BrowserHistorySimulator {
     public static void main(String[] args) {
+        //Create a Stack
         Stack<String> browserHistorySimulator = new Stack<>();
+
+
+
 
 
     }
