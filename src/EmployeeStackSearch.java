8e2b5360e94132236cf4df6fb2c1cfce96bee116
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-23 11:34:11 +0400

    Completed Task2 employeeStackSearch

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -13,41 +13,33 @@ public class EmployeeStackSearch {
         employeeStackSearch.push("Muhammad");
         employeeStackSearch.push("Riham");
         employeeStackSearch.push("Rana");
-            //Display all employee
+        //Display all employee
         System.out.println("All Employees :" + employeeStackSearch);
-            //search for exit element using search
+        //search for exit element using search
         int searchResult = employeeStackSearch.search("Ahmed"); // search by name
         System.out.println("Position of Ahmed from top: " + searchResult);
-          //Check if element exit in  Stack
-        System.out.println("If employee Fatima exit ? "+ employeeStackSearch.contains("Fatima"));
-            //Display all elements using for loop
+        //Check if element exit in  Stack
+        System.out.println("If employee Fatima exit ? " + employeeStackSearch.contains("Fatima"));
+        //Display all elements using for loop
         System.out.println("Use for loop to display all employees ");
-        for (int i=0;i<employeeStackSearch.size();i++) {
+        for (int i = 0; i < employeeStackSearch.size(); i++) {
             System.out.println("  Position " + i + ": " + employeeStackSearch.get(i));
-        //Display all element using for each loop
-        System.out.println("Use for each loop to display all employees ");
-
-        for (String name:employeeStackSearch){
-            System.out.println("All employees :" +name);
-
+            //Display all element using for each loop
         }
-         //traverse all element
-        System.out.println("Use an Iterator to traverse the  employees");
+            System.out.println("Use for each loop to display all employees ");
+
+            for (String name : employeeStackSearch) {
+                System.out.println("All employees :" + name);
+
+            }
+            //traverse all element
+            System.out.println("Use an Iterator to traverse the  employees");
 
             for (int j = employeeStackSearch.size() - 1; j >= 0; j--) {
-            System.out.println("  Position " + j + ": " + employeeStackSearch.get(j));
+                System.out.println("  Position " + j + ": " + employeeStackSearch.get(j));
+            }
+
+
         }
-
-
-
-
-
-
-
-
-
-
-
-
     }
-}
+
