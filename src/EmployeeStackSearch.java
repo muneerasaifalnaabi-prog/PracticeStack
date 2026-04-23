1e2f0ac57f6e291a3be4fc508d3c133bd540799f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-23 10:58:10 +0400

    Added a name  for stack

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -2,6 +2,6 @@ import java.util.Stack;
 
 public class EmployeeStackSearch {
     public static void main(String[] args) {
-        Stack<String> employeeStackSearch
+        Stack<String> employeeStackSearch = new Stack<>();
     }
 }
