b74d922bf46ba1cde432451b4a34b5213da94915
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-25 11:01:39 +0400

    Updated in class StringReversalAndPalindrome

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -2,9 +2,15 @@ import java.util.Stack;
 
 public class StringReversalAndPalindrome {
     public static void main(String[] args) {
-        //create Stack Store characther
-        Stack<Character> stack = new Stack<>();
-
+        String s1="mom";
+        String s2="dad";
+        String s3="abcd";
+        String s4="hello";
+        String s5="level";
+        String s6="java";
+        System.out.println("Original String :"+s3);
+        System.out.println("Reverse String :"+reverseString(s3));
+        System.out.println(isPalindrome(s3));
 
     }
     public static String reverseString(String s) {
@@ -12,12 +18,12 @@ public class StringReversalAndPalindrome {
         String result = "";
         for (int i = 0; i < s.length(); i++) {
             stack.push(s.charAt(i));
-            result += stack.peek();
-            stack.pop();
 
         }
+        for (int i = 0; i < s.length(); i++) {
+            result += stack.pop();
+        }
 
-       result += stack.peek();
         return result;
     }
     public static Boolean isPalindrome(String s) {
@@ -29,7 +35,6 @@ public class StringReversalAndPalindrome {
             }
             left++;
             right--;
-
         }
         return true;
     }
