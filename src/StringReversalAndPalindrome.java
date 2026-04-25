631b1c2e7d8551a433e1fee5f81830f2e41d6d09
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-25 10:31:50 +0400

    Added Method isPalindrome to check the string is isPalindrome or not

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -20,4 +20,17 @@ public class StringReversalAndPalindrome {
        result += stack.peek();
         return result;
     }
+    public static Boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+
+        }
+        return true;
+    }
 }
