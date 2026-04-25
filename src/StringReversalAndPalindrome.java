09c16ffffc9a08173b47778af74c2d0fb572363b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-25 10:14:58 +0400

    Added Stack with char data type

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -1,6 +1,9 @@
+import java.util.Stack;
+
 public class StringReversalAndPalindrome {
     public static void main(String[] args) {
         //create Stack Store characther
+        Stack<Character> stack = new Stack<>();
 
     }
 }
