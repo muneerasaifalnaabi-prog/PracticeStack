3981c9c1e528f644bb8378f47f8a9e2e2a002dd8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-25 11:45:52 +0400

    Added method decimalToBinary

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -1,8 +1,26 @@
+import java.util.Stack;
+
 public class DecimalToBinaryConverter {
     public static void main(String[] args) {
 
     }
     public static String decimalToBinary(int num) {
+        Stack<Integer> stack = new Stack<>();
+        String binary = "";
+        if (num == 0) {
+            return "0";
+        }
+        while (num != 0) {
+            stack.push(num%2);
+                num = num / 2;
+                if (num<0) {
+                    num = num*-1;
+                }
+        }
+        for (int i = 0; i < stack.size(); i++) {
+            binary = binary + stack.pop();
+        }
+        return binary;
 
     }
 }
