f5461bf680a3aeb5a8e2b9895b54678562de7c0f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 16:33:31 +0400

    completed infix logic

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -5,6 +5,12 @@ public class InfixToPostfixConverter {
 
 
     }
+    public static int getPrecedence(char op) {
+        if (op == '^')                           return 3;
+        if (op == '*' || op == '/' || op == '%') return 2;
+        if (op == '+' || op == '-')              return 1;
+        return 0;
+    }
     public static String infixToPostfix(String infix) {
         Stack<Character> stack = new Stack<>();
         String postfix="";
@@ -18,7 +24,7 @@ public class InfixToPostfixConverter {
             char c =infix.charAt(i);
 
             if (c==' '){
-                continue;;
+                continue;
             }
             if (Character.isDigit(c)){
                 postfix+=c +"";
@@ -36,9 +42,24 @@ public class InfixToPostfixConverter {
                     stack.pop();
                 }
         }
+            else {
+                if (stack.size() < 0) {
+                    System.out.println("Invalid expression");
+                    return null;
+                }
+                while (!stack.isEmpty() && stack.peek() != '(' &&
+                        getPrecedence(stack.peek()) >= getPrecedence(c)) {
+                    postfix += stack.pop() + " ";
+                }
+                stack.push(c);
+            }}
+        while (!stack.isEmpty()) {
+            postfix += stack.pop() + " ";
+        }
 
-
-
-
+        return postfix.trim();
     }
+
+
 }
+
