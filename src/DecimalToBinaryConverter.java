5ab316e1967cdc75a8b4c709dc22e7971825f143
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 09:03:00 +0400

    Fixed bugs and Completed Task7  DecimalToBinaryConverter

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -1,54 +1,54 @@
 import java.util.Stack;
-/*
 
 
-==============================
-It need to check :
-=============================
-
- */
 public class DecimalToBinaryConverter {
+
     public static void main(String[] args) {
-        int num =10;
+        int num = 5;
         System.out.println(decimalToBinary(num));
-
-       // displayConversionProcess(num);
-
-
+        displayConversionProcess(num);
     }
+
     public static String decimalToBinary(int num) {
         Stack<Integer> stack = new Stack<>();
         String binary = "";
+
         if (num == 0) {
             return "0";
         }
-        while (num > 0) {
-           int reminder =num%2;
-           stack.push(reminder);
-                num = num / 2;
 
+        while (num > 0) {
+            int reminder = num % 2;
+            stack.push(reminder);
+            num = num / 2;
         }
-        for (int i = 0; i < stack.size(); i++) {
+
+
+        while (!stack.isEmpty()) {
             binary = binary + stack.pop();
         }
-        return binary;
 
+        return binary;
     }
+
     public static void displayConversionProcess(int num) {
         if (num == 0) {
-            System.out.println("Special case"+num);
+            System.out.println("Special case" + num);
             return;
         }
-        Stack<Integer> stack = new Stack<>();
-        System.out.println("Conversion process step for "+num+":");
-        while (num != 0) {
-            System.out.println("Reminder :"+ stack.push(num%2));
-            System.out.println("Quotient :"+ num/2);
-            if (num<0) {
-                num = num*-1;
-            }
+
+
+        if (num < 0) {
+            num = num * -1;
         }
 
+        Stack<Integer> stack = new Stack<>();
+        System.out.println("Conversion process step for " + num + ":");
 
+        while (num != 0) {
+            System.out.println("Reminder :" + stack.push(num % 2));
+            System.out.println("Quotient :" + num / 2);
+            num = num / 2;
+        }
     }
 }
