b9a5f7cfeb940fa409bc6e328f2ad95e30dc8a3c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 16:20:41 +0400

    Added edge case

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -1,9 +1,16 @@
+import java.util.Stack;
+
 public class InfixToPostfixConverter {
     public static void main(String[] args) {
 
 
     }
     public static String infixToPostfix(String infix) {
+        Stack<Character> stack = new Stack<>();
+        String postfix="";
+        //edge case :if empty or null
+
+
 
     }
 }
