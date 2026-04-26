8cea0762eb57aacdf8605bacd04cca16298ce997
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 12:22:29 +0400

    Added 3 Stackes for pages

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -1,4 +1,11 @@
+import java.util.Stack;
+
 public class TowerOfHanoiSolver {
+    static Stack<Integer> pegA = new Stack<>();
+    static Stack<Integer> pegB = new Stack<>();
+    static Stack<Integer> pegC = new Stack<>();
+
+    static int count=0;
     public static void main(String[] args) {
 
     }
