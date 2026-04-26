5de5972c56176f8e51060330e6c020330239cb91
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 14:25:43 +0400

    Added visitPage method

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -1,5 +1,21 @@
+import java.util.Stack;
+
 public class BrowserNavigationSystem {
+    static Stack<String> back=new Stack<>();
+    static Stack<String> farwared=new Stack<>();
+    static String currentPage="";
+    static int  max=10;
     public static void main(String[] args) {
 
+
+    }
+    static  void visitPage(String url){
+        //limit back history to max only
+        if (back.size()>=max){
+            System.out.println("History limit reached ");
+            back.remove(0);
+
+        }
+        if (currentPage.)
     }
 }
