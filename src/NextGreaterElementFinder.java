e89f6cd29f1a6b285836798fac1dfc4da8543a08
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 23:36:38 +0400

    Updated findNextGreaterElement method

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -7,6 +7,16 @@ public class NextGreaterElementFinder {
     public static int[] findNextGreaterElement(int[] arr) {
         int[] result = new int[arr.length];
         Stack<Integer> stack = new Stack<>();
+        // While current element is greater than what the top index holds,
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[i] > stack.peek()) {
+                int pop = stack.pop();
+                result[pop] = arr[i];
+            }
+            stack.push(i);//push current index
+        }
+        stack.clear();   // clear() when done
+        return result;
 
     }
 }
