17fab725e51f8c42b1149380e849a519d43de2ef
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 00:09:21 +0400

    Added enqueue method

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -2,11 +2,15 @@ import java.util.Stack;
 import java.util.NoSuchElementException;
 
 public class QueueUsingTwoStacks {
-    static Stack<T> input =new Stack<>();
+     static Stack<T> input =new Stack<>();
     static Stack<T> output =new Stack<>();
     public static void main(String[] args) {
 
 
     }
-    p
+    public <T> void enqueue(T element){
+        input.push(element);
+        so
+
+    }
 }
