9bbf8ba88cb31e00e0ad2cef4d15c4fc8dba6957
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 11:25:49 +0400

    validateHtmlTags method added

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -1,5 +1,12 @@
+import java.util.Stack;
+
 public class HtmlTagValidator {
     public static void main(String[] args) {
+
+    }
+    public static Boolean  validateHtmlTags(String html){
+        Stack<String> stack=new Stack<>();
+
 
     }
 
