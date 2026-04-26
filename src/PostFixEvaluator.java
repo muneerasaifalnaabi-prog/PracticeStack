63ff28cc389a3ed2cbaf99b13974c6dc3e747ebd
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 09:13:01 +0400

    Implemented  method evaluatePostfix

--- src/PostFixEvaluator.java
+++ src/PostFixEvaluator.java
@@ -1,9 +1,15 @@
+import java.util.Stack;
+
 public class PostFixEvaluator {
     public static void main(String[] args) {
 
 
     }
     public static Integer evaluatePostfix(String expression){
+        Stack<Integer> stack=new Stack<>();
+        for ()
+
+
 
     }
 }
