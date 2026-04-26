abf9165cebe9970ea0b3c326bfc783f8a2279cdc
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 00:17:50 +0400

    Completed task15 QueueUsingTwoStacks

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -1,30 +1,60 @@
 import java.util.Stack;
 
-public class QueueUsingTwoStacks {
-     static Stack<T> input =new Stack<>();
-    static Stack<T> output =new Stack<>();
+public class QueueUsingTwoStacks<T> {
+    // Stacks should be instance variables, not static
+    private Stack<T> input = new Stack<>();
+    private Stack<T> output = new Stack<>();
+
     public static void main(String[] args) {
+        QueueUsingTwoStacks<Integer> queue = new QueueUsingTwoStacks<>();
 
+        int[] ops = {10, 20, 30, -1, 40, -2, 50, -1, -1, 60, -1, -1};
 
+        for (int op : ops) {
+            if (op == -1) {
+                if (!queue.isEmpty()) System.out.println("Dequeued: " + queue.dequeue());
+            } else if (op == -2) {
+                if (!queue.isEmpty()) System.out.println("Peeked: " + queue.peek());
+            } else {
+                queue.enqueue(op);
+            }
+            queue.displayStatus();
+        }
     }
-    public <T> void enqueue(T element){
+
+    public void enqueue(T element) {
         input.push(element);
         System.out.println("Enqueued: " + element);
-
     }
-    public <T> T dequeue() {
-        if (input.isEmpty()) {
 
+    public T dequeue() {
+        shiftStacks();
+        return output.isEmpty() ? null : output.pop();
+    }
+
+    public T peek() {
+        shiftStacks();
+        return output.isEmpty() ? null : output.peek();
+    }
+
+    // Helper method to move elements only when output is empty
+    private void shiftStacks() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
         }
-        return output.pop();
     }
-    public static boolean isEmpty() {
+
+    public boolean isEmpty() {
         return input.isEmpty() && output.isEmpty();
     }
-    public static int size() {
+
+    public int size() {
         return input.size() + output.size();
     }
 
-
+    public void displayStatus() {
+        System.out.println("Input: " + input + " | Output: " + output);
     }
 }
