63c80aefb59c308c809c0a98315fcfa191946997
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 23:31:36 +0400

    Updated findNextGreaterElement method

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -1,8 +1,12 @@
+import java.util.Stack;
+
 public class NextGreaterElementFinder {
     public static void main(String[] args) {
 
     }
     public static int[] findNextGreaterElement(int[] arr) {
+        int[] result = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
 
     }
 }
