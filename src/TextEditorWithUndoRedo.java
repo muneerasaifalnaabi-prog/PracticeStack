2c7cb083cc8c2d91d84ed29c52865ace0ab67a93
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 10:40:42 +0400

    tryed to fix bugs

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,10 +1,33 @@
+import java.util.Stack;
+
 public class TextEditorWithUndoRedo {
+    static  Stack<String> undo = new Stack<>();
+    static   Stack<String> redo = new Stack<>();
+    String currentState ="";
     public static void main(String[] args) {
 
 
     }
-    public  static String type(String text ){
+    public  static void type(String text ){
+
+
+        undo.push(text);
+        currentState+=text;
+        redo.clear();
 
     }
+    public  static void undo(){
+        if (!undo.isEmpty()) {
+            redo.push(currentState);
+            currentState = undo.pop();
+        }
+    }
+    private void redo() {
+        if (!redo.isEmpty()) {
+            undo.push(currentState);
+            currentState = redo.pop();
+        }
+    }
+
 
 }
