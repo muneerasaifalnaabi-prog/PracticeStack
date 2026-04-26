3058b5e7ce3f4519e9f67a247b60bd8ba263d8f0
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 00:06:56 +0400

    Added class QueueUsingTwoStacks

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -1,5 +1,12 @@
+import java.util.Stack;
+import java.util.NoSuchElementException;
+
 public class QueueUsingTwoStacks {
+    static Stack<T> input =new Stack<>();
+    static Stack<T> output =new Stack<>();
     public static void main(String[] args) {
 
+
     }
+    p
 }
