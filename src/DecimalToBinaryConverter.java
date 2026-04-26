4b3e202ffb83bf206f0356a593d86a252f51fd7b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-26 10:13:13 +0400

    added binaryToDesimal method to DecimalToBinaryCon

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -4,9 +4,11 @@ import java.util.Stack;
 public class DecimalToBinaryConverter {
 
     public static void main(String[] args) {
-        int num = 5;
-        System.out.println(decimalToBinary(num));
-        displayConversionProcess(num);
+        //int num = 5;
+       // System.out.println(decimalToBinary(num));
+       // displayConversionProcess(num);
+        String binary ="1011";
+        System.out.println(binaryToDesimal(binary));
     }
 
     public static String decimalToBinary(int num) {
@@ -50,5 +52,22 @@ public class DecimalToBinaryConverter {
             System.out.println("Quotient :" + num / 2);
             num = num / 2;
         }
+    }
+        public static Integer binaryToDesimal(String binary){
+        Stack<Integer> stack =new Stack<>();
+        for (int i=0;i<binary.length();i++){
+            stack.push(Character.getNumericValue(binary.charAt(i)));
+        }
+        int decimal=0;
+        int power =0;
+
+        while (!stack.isEmpty()){
+            int pop = stack.pop();
+            decimal+=pop*Math.pow(2,power);
+            power++;
+        }
+        return decimal;
+
+
     }
 }
