50bccf69cc0e50af472c14f6a20dd484d6729175
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 15:17:54 +0400

    added CelebrityFinder class

--- src/CelebrityFinder.java
+++ src/CelebrityFinder.java
@@ -1,6 +1,11 @@
+import java.util.Stack;
+
 public class CelebrityFinder {
     public static void main(String[] args) {
 
+    }
+    public static int findCelebrity(int n ){
+        Stack<Integer> stack =new Stack<>();
     }
 
 }
