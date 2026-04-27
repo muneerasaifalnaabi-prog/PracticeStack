fef8ffe0d608cc7195419b71747ce32189733c22
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 10:11:55 +0400

    Added evaluate method

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -1,5 +1,14 @@
+import java.util.Stack;
+
 public class InfixExpressionEvaluator {
+    static Stack<Double> operandStack =new Stack<>();
+    static Stack<Character> operatorStack =new Stack<>();
     public static void main(String[] args) {
 
+
     }
+    public static String evaluate(String expression) {
+
+    }
+
 }
