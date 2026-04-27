8fe80142b5197b0de139e2329983a6a038053adf
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 15:22:58 +0400

    added CelebrityFinder class

--- src/CelebrityFinder.java
+++ src/CelebrityFinder.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class CelebrityFinder {
+    static ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
     public static void main(String[] args) {
 
     }
+    public static Boolean knows(int a ,int b){
+        return matrix.get(a).get(b)==1;
+    }
+
+
+
     public static int findCelebrity(int n ){
         Stack<Integer> stack =new Stack<>();
 
@@ -12,6 +20,12 @@ public class CelebrityFinder {
             stack.push(n);
         }
         System.out.println("Initial stack: " + stack);
+
+        while (stack.size() >= 2) {
+            int a = stack.pop();
+            int b = stack.pop();
+
     }
+
 
 }
