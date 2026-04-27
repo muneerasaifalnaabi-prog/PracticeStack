6d43a608b45782c3175eca3a0af9cc96c4e4e272
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 10:49:09 +0400

    updated findLargestRectangle method

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -1,9 +1,18 @@
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
     public static void main(String[] args) {
 
     }
 
     public static int findLargestRectangle(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        if (heights.length == 0) {
+            return 0;
+        }
+
+
 
     }
 
