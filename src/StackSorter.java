a6d911e3d6eeae5fd50bfb87a31d078436acd2f8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 10:02:01 +0400

    Test case

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -2,6 +2,19 @@ import java.util.Stack;
 
 public class StackSorter {
     public static void main(String[] args) {
+        System.out.println("===== Test 1: Normal Unsorted =====");
+        Stack<Integer> stack1 = new Stack<>();
+        stack1.push(3);
+        stack1.push(1);
+        stack1.push(4);
+        stack1.push(1);
+        stack1.push(5);
+        System.out.println("Before :");
+        displayStack( stack1);
+        sortStack(stack1);
+        displayStack(stack1);
+        System.out.println();
+
 
     }
     public static void sortStack(Stack<Integer> stack) {
@@ -37,6 +50,9 @@ public class StackSorter {
         int value = stack.pop();
         insertInSortedOrder(stack, top);
         stack.push(top);
+    }
+    public static void displayStack(Stack<Integer> stack) {
+        System.out.println( stack);
     }
 
 }
