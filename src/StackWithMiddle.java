8d951e51c8e53bcba37a44268a2387f501fc2f52
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 14:36:55 +0400

    Added node class

--- src/StackWithMiddle.java
+++ src/StackWithMiddle.java
@@ -1,5 +1,20 @@
 public class StackWithMiddle {
     //node class
+    static class Node {
+        int  data;
+        Node next;
+        Node prev;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+        //variables
+        static Node top  = null;
+
+
+    }
     public static void main(String[] args) {
 
     }
