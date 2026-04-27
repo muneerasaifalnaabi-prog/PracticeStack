8a8aed17805d27dc511e6590a77d8c8ba17723c2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 08:59:11 +0400

    added push and pop method

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,5 +1,24 @@
+import java.util.Stack;
+
 public class MinStack {
+    static Stack<Integer> mainStack =new Stack<>();
+    static Stack<Integer> minStack =new Stack<>();
     public static void main(String[] args) {
 
     }
+    public static void  push(int value){
+        mainStack.push(value);
+        if (mainStack.isEmpty() || value<=minStack.peek()){
+            minStack.push(value);
+        }
+    }
+    public static void pop(int value){
+       int pop= mainStack.pop();
+        if (pop ==minStack.peek()){
+            minStack.pop();
+        }
+
+    }
+
+
 }
