ce7fe98769d23501c169fd0ec2411d0703ebfb76
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 11:56:08 +0400

    printed each raw

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -107,7 +107,13 @@ public class NQueensSolver {
             System.out.print(j + " ");
         }
         // Print each row
-
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
 
 
 
