aea8eb842f82eac39f819a02731a1ec28e770cd1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 11:33:30 +0400

    Added position class

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -3,7 +3,10 @@ public class NQueensSolver {
     static class Position {
         int row;
         int col;
-
+        Position(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
 
     }
     public static void main(String[] args) {
