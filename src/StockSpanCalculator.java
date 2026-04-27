2e5eaea0218f6339a14e741638bfcd67fe49a56d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-27 13:29:00 +0400

    updated calculateSpan method

--- src/StockSpanCalculator.java
+++ src/StockSpanCalculator.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class StockSpanCalculator {
     public static void main(String[] args) {
 
@@ -5,6 +7,9 @@ public class StockSpanCalculator {
     // Simple O(n) stack method
     public static int[] calculateSpan(int[] prices) {
         int[] span = new int[prices.length];
+
+        Stack<Integer> stack = new Stack<>();
+
 
     }
 }
