ac8ac394b6281fd9fa82e42432cf39d93d79e6df
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-04-28 09:00:01 +0400

    updated class parenthesesValidator

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -17,6 +17,8 @@ public class ParenthesesValidator {
     public static  Boolean  isBalanced(String expression) {
        Stack<Character> stack = new Stack<>();
         System.out.println("Validating " + expression);
+
+        if (!expression.isEmpty())
         for (int i = 0; i < parenthesesValidator.size(); i++) {
             char c = expression.charAt(i);
             if (parenthesesValidator.isEmpty()) {
